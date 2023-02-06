9523caf439e7315e40f0838a708fca546469f167
niph <muhammadhnf97@gmail.com>
2023-02-06 19:20:52 +0700

    fixed some bugs in detail

--- app/property/[id]/page.jsx
+++ app/property/[id]/page.jsx
@@ -10,60 +10,67 @@ import PropertyCard from "@/app/propertyCard"
 import { nanoid } from "nanoid"
 
 export default function Details({params : {id}}){
-    const detailPerumahan = dataPerumahan.filter(data=>data.id == id).reduce((obj, item) =>{
-        obj[item.key] = item.value;
+    const detailPerumahan = dataPerumahan.filter(data=>data.id == id)
+    const detail = detailPerumahan?.reduce((obj, item) =>{
+        return obj[item]
     })
-
     const formatter = new Intl.NumberFormat('id-ID', {style: 'currency', currency: 'IDR',})
     return (
-        <div className="w-full px-5 md:max-w-7xl md:mx-auto mt-[4rem]">
-            <div className="w-full md:h-[30rem] md:flex md:gap-3 md:mb-5">
-                <div className="relative w-full h-52 md:h-full flex-1">
-                    <Image src={detailPerumahan.thumbnail} alt='thumbnails' fill className="object-cover" />
-                </div>
-                <div className="md:w-[30rem] md:h-full md:flex md:flex-col">
-                    <h3 className="font-semibold border-b-2 border-red-600 mb-1 text-md w-fit md:text-lg">Gallery</h3>
-                    <div className="grid grid-cols-3 md:grid-cols-2 w-full max-h-24 md:max-h-full md:max-w-[30rem] gap-1 overflow-y-scroll bg-white p-1">
-                        {detailPerumahan.images.map(data=>{
-                            return (
-                                <div key={nanoid()} className="relative w-full h-24 md:h-32">
-                                    <Image src={data} alt='images' fill className="object-cover" />
-                                </div>
-                            )
-                        })}
+        <>
+        {dataPerumahan.filter(data=>data.id == id).map(datarumah=>{
+            return (
+            <div key={datarumah.id} className="w-full px-5 md:max-w-7xl md:mx-auto mt-[4rem]">
+                <div className="w-full md:h-[30rem] md:flex md:gap-3 md:mb-5">
+                    <div className="relative w-full h-52 md:h-full flex-1">
+                        <Image src={datarumah.thumbnail} alt='thumbnails' fill className="object-cover" />
+                    </div>
+                    <div className="md:w-[30rem] md:h-full md:flex md:flex-col">
+                        <h3 className="font-semibold border-b-2 border-red-600 mb-1 text-md w-fit md:text-lg">Gallery</h3>
+                        <div className="grid grid-cols-3 md:grid-cols-2 w-full max-h-24 md:max-h-full md:max-w-[30rem] gap-1 overflow-y-scroll bg-white p-1">
+                            {datarumah.images.map(data=>{
+                                return (
+                                    <div key={nanoid()} className="relative w-full h-24 md:h-32">
+                                        <Image src={data} alt='images' fill className="object-cover" />
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="w-full h-fit bg-white rounded-sm shadow-sm px-3 py-3 my-5">
-                <p className="font-semibold text-2xl">{detailPerumahan?.nama}</p>
-                <p>{detailPerumahan?.kota}</p>
-                <div className="flex items-center"><span><BiMap className="text-red-700" /></span><p>{detailPerumahan?.alamat}</p></div>
-                <div className="mt-2 grid grid-cols-2 border px-3 py-1 rounded-md my-2">
-                    <p>DP</p>
-                    <p>{formatter.format(detailPerumahan.dp)}</p>
-                    <p>Harga Rumah</p>
-                    <p>{formatter.format(detailPerumahan.harga)}</p>
-                    <p className='flex items-center gap-2 w-full'><BiBed className="text-red-700" />Kamar Tidur</p>
-                    <p>{detailPerumahan?.desc.kt}</p>
-                    <p className='flex items-center gap-2 w-full'><FaShower className="text-red-700" />Kamar Mandi</p>
-                    <p>{detailPerumahan?.desc.km}</p>
-                    <p className='flex items-center gap-2 w-full'><BsHouseDoor className="text-red-700"/>Luas Bangunan</p>
-                    <p>{detailPerumahan?.desc.lb}m <sup>2</sup></p>
-                    <p className='flex items-center gap-2 w-full'><MdTerrain className="text-red-700"/>Luas Tanah</p>
-                    <p>{detailPerumahan?.desc.lt}m <sup>2</sup></p>
-                    <p className='flex items-center gap-2 w-full'><BsHouseFill className="text-red-700"/>Total Unit</p>
-                    <p>{detailPerumahan?.tot_unit} Unit</p>
+                <div className="w-full h-fit bg-white rounded-sm shadow-sm px-3 py-3 my-5">
+                    <p className="font-semibold text-2xl">{datarumah?.nama}</p>
+                    <p>{datarumah?.kota}</p>
+                    <div className="flex items-center"><span><BiMap className="text-red-700" /></span><p>{datarumah?.alamat}</p></div>
+                    <div className="mt-2 grid grid-cols-2 border px-3 py-1 rounded-md my-2">
+                        <p>DP</p>
+                        <p>{formatter.format(datarumah.dp)}</p>
+                        <p>Harga Rumah</p>
+                        <p>{formatter.format(datarumah.harga)}</p>
+                        <p className='flex items-center gap-2 w-full'><BiBed className="text-red-700" />Kamar Tidur</p>
+                        <p>{datarumah?.desc.kt}</p>
+                        <p className='flex items-center gap-2 w-full'><FaShower className="text-red-700" />Kamar Mandi</p>
+                        <p>{datarumah?.desc.km}</p>
+                        <p className='flex items-center gap-2 w-full'><BsHouseDoor className="text-red-700"/>Luas Bangunan</p>
+                        <p>{datarumah?.desc.lb}m <sup>2</sup></p>
+                        <p className='flex items-center gap-2 w-full'><MdTerrain className="text-red-700"/>Luas Tanah</p>
+                        <p>{datarumah?.desc.lt}m <sup>2</sup></p>
+                        <p className='flex items-center gap-2 w-full'><BsHouseFill className="text-red-700"/>Total Unit</p>
+                        <p>{datarumah?.tot_unit} Unit</p>
+                    </div>
+                    <iframe src={datarumah?.maps} width="600" height="450" loading="lazy" className="w-full max-h-72 rounded-md"></iframe>
+                    {datarumah?.onprogress && <button className="px-2 py-2 bg-blue-500 rounded-sm my-2 text-white hover:bg-blue-700 text-sm duration-200">Jadwalkan untuk Survey !</button>}
                 </div>
-                <iframe src={detailPerumahan?.maps} width="600" height="450" loading="lazy" className="w-full max-h-72 rounded-md"></iframe>
-                {detailPerumahan?.onprogress && <button className="px-2 py-2 bg-blue-500 rounded-sm my-2 text-white hover:bg-blue-700 text-sm duration-200">Jadwalkan untuk Survey !</button>}
-            </div>
-            <div className="w-full h-fit my-10">
-            <h2 className="font-bold text-lg">Property Lainnya</h2>
-            <div className="grid grid-cols-3 gap-3">
-                <PropertyCard listPerumahan={dataPerumahan.filter(data=>data.onprogress)} />
-            </div>
+                <div className="w-full h-fit my-10">
+                <h2 className="font-bold text-lg">Property Lainnya</h2>
+                <div className="grid grid-cols-3 gap-3">
+                    <PropertyCard listPerumahan={dataPerumahan.filter(data=>data.onprogress)} />
+                </div>
 
+                </div>
             </div>
-        </div>
+            )
+        })
+        }
+        </>
     )
 }
